1eceb0b63e83874d434ecd73ee89b72ddfd4294c
diegodemello <diegodemello027@gmail.com>
2025-07-26 00:41:10 -0300

    feat: cria a pagina de login de usuário.

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './login.css'
 })
 export class Login {
+  constructor(private router : Router){
 
+  }
+
+  returnPage(): void {
+    this.router.navigate(['/']);
+  }
 }
