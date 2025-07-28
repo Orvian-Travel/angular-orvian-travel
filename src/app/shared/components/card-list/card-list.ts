d575a2b082f91cd1c8c8ce0b26131f1bd8d63346
diegodemello <diegodemello027@gmail.com>
2025-07-28 10:22:28 -0300

    feat: altera o componente card para receber dados da API.

--- src/app/shared/components/card-list/card-list.ts
+++ src/app/shared/components/card-list/card-list.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { Card } from "../card/card";
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Package as PackageEntity } from '../../../services/entities/package';
+import { Package as PackageService } from '../../../services/api/package/package';
 
 @Component({
   selector: 'app-card-list',
-  imports: [Card],
+  imports: [CommonModule],
   templateUrl: './card-list.html',
   styleUrl: './card-list.css'
 })
-export class CardList {
+export class CardList implements OnInit {
+    constructor(private router: Router, private service: PackageService ){
 
+  }
+    packages: PackageEntity[] = [];
+
+  ngOnInit(): void {
+    this.service.getAllPackages().subscribe((response: PackageEntity[]) => this.packages = response);
+  }
+
+
+
+  navigateToDetails(){
+    this.router.navigate(['/product-details']);
+  }
 }
