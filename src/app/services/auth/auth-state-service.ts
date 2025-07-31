a2f546c5c11216d001d10dd2355f4e33716e8097
Leonardo Lima <leo2342005@gmail.com>
2025-07-31 15:30:20 -0300

    feat: adicionando um método para obter o id do usuário, que está presente no JWT Token.

--- src/app/services/auth/auth-state-service.ts
+++ src/app/services/auth/auth-state-service.ts
@@ -57,6 +57,11 @@ export class AuthStateService {
     }
   }
 
+  getUserId(): string | null {
+    const decoded = this.getDecodedToken();
+    return decoded?.sub || null;
+  }
+
   getUserRole(): string | null {
     const decoded = this.getDecodedToken();
     return decoded?.role || null;
