57bb7aec5955de782461b9b2ba2cde6c02a4a8a7
Silas Henrique <silas.henrique21@outlook.com>
2025-08-01 12:10:19 -0300

    feat: Criação da seção de apresentação da orvian com gif de explicação do logo

---
+++ src/app/shared/components/presentation-video/presentation-video.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation-video',
+  templateUrl: './presentation-video.html',
+  styleUrl: './presentation-video.css',
+  standalone: true
+})
+export class PresentationVideoComponent {
+
+}
