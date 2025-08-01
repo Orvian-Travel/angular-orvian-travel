29fc5bc1fb0f99d3d148d8c7b54beeee916d72a0
DaltonGuima <dalton.guimaraes500@gmail.com>
2025-07-31 21:21:11 -0300

    minor-change: deixei reservations-list, preparado para chamada da api, porém ainda não implementada no back-end

--- src/app/pages/reservations/components/reservations-list/reservations-list.ts
+++ src/app/pages/reservations/components/reservations-list/reservations-list.ts
@@ -9,18 +9,44 @@ import { Component } from '@angular/core';
 })
 export class ReservationsList {
   selectedStatus: string = 'todas';
+  reservations: any[] = [];
+  currentPage: number = 0;
+  pageSize: number = 10;
+  totalElements: number = 0;
+  totalPages: number = 0;
 
   onStatusFilterChange(status: string): void {
     this.selectedStatus = status;
-    this.filterReservationsByStatus(status);
+    this.currentPage = 0;
+    this.loadReservations();
   }
 
-  private filterReservationsByStatus(status: string): void {
+  private loadReservations(): void {
     // Aqui você fará a requisição para o backend
     // Exemplo:
     // this.reservationService.getReservationsByStatus(status).subscribe(...)
+    const statusParam = this.selectedStatus === '' ? undefined : this.selectedStatus;
 
-    console.log(`Filtrar reservas por status: ${status}`);
+    console.log(`Filtrar reservas por status: ${statusParam}`);
     // TODO: Implementar chamada para API
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.loadReservations();
+  }
+
+  onNextPage(): void {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+      this.loadReservations();
+    }
+  }
+
+  onPreviousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.loadReservations();
+    }
   }
 }
