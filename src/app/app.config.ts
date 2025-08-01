a37a800301353a24862ed40ca767584faade6473
Leonardo Lima <leo2342005@gmail.com>
2025-08-01 09:46:45 -0300

    refactor: atualizando para o AuthInterceptor agir antes do loadingInterceptor.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,7 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withInMemoryScrolling({
       scrollPositionRestoration: 'enabled'
     })),
-    provideHttpClient(withInterceptors([loadingInterceptor, AuthInterceptor])),
+    provideHttpClient(withInterceptors([AuthInterceptor, loadingInterceptor])),
     { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
