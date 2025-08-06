292a87b1eb0abf45bfbc45e640148445095c38a5
Leonardo Lima <leo2342005@gmail.com>
2025-08-05 21:16:11 -0300

    feat: adicionando o chatbot a pagina home.

--- src/app/pages/home/home/home.ts
+++ src/app/pages/home/home/home.ts
@@ -5,10 +5,11 @@ import { CardList } from '../../../shared/components/card-list/card-list';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { PresentationVideoComponent } from '../../../shared/components/presentation-video/presentation-video';
 import { SearchData } from '../../../services/entities/search-data.model';
+import { ChatWidgetComponent } from "app/chatbot/components/chat-widget/chat-widget.component";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Hero, CardList, PresentationVideoComponent, FooterComponent],
+  imports: [Header, Hero, CardList, PresentationVideoComponent, FooterComponent, ChatWidgetComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
